9400cd341c7010891232e4aae354a85a255ced81
peddyrbeg <85888409+peddyrbeg@users.noreply.github.com>
2024-01-26 17:03:30 +0000

    Add files via upload

---
+++ fysseree/focklyn.js
@@ -0,0 +1,38 @@
+const focklynYnsit = {
+    "braew": {
+        caslys: "caslyssyn/braew.webp",
+        sheean: "sheeanyn/braew.mp3"
+    },
+    "çheh": {
+        caslys: "caslyssyn/cheh.webp",
+        sheean: "sheeanyn/cheh.mp3"
+    },
+    "feayr": {
+        caslys: "caslyssyn/feayr.webp",
+        sheean: "sheeanyn/feayr.mp3"
+    },
+    "sterrymagh": {
+        caslys: "caslyssyn/sterrymagh.webp",
+        sheean: "sheeanyn/sterrymagh.mp3"
+    },
+    "grianagh": {
+        caslys: "caslyssyn/grianagh.webp",
+        sheean: "sheeanyn/grianagh.mp3"
+    },
+    "bodjallagh": {
+        caslys: "caslyssyn/bodjallagh.webp",
+        sheean: "sheeanyn/bodjallagh.mp3"
+    },
+    "geayagh": {
+        caslys: "caslyssyn/geayagh.webp",
+        sheean: "sheeanyn/geayagh.mp3"
+    },
+    "ceau fliaghey": {
+        caslys: "caslyssyn/ceau fliaghey.webp",
+        sheean: "sheeanyn/ceau fliaghey.mp3"
+    },
+    "ceau sniaghtey": {
+        caslys: "caslyssyn/ceau sniaghtey.webp",
+        sheean: "sheeanyn/ceau sniaghtey.mp3"
+    }
+};
